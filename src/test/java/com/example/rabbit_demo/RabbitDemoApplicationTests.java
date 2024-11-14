6d2f5a0f91075644847d3e99a47e16b6559eb618
vladimir.surcov <vladimir.surcov@dsi.md>
2024-11-14 17:49:17 +0200

    Initial commit

---
+++ src/test/java/com/example/rabbit_demo/RabbitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbit_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
