d4d0ef12b85090174c5e4505cb877d26d01673f5
vladimir.surcov <vladimir.surcov@dsi.md>
2024-12-29 12:20:59 +0200

    add RabbitListenerTestHarness test

--- src/test/java/com/example/rabbit_demo/listenerTests/FooBarTests2.java
+++ src/test/java/com/example/rabbit_demo/listenerTests/FooBarTests2.java
@@ -1,6 +1,5 @@
 package com.example.rabbit_demo.listenerTests;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.core.AnonymousQueue;
 import org.springframework.amqp.core.Queue;
@@ -13,7 +12,6 @@ import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.test.RabbitListenerTest;
 import org.springframework.amqp.rabbit.test.RabbitListenerTestHarness;
-import org.springframework.amqp.rabbit.test.mockito.LatchCountDownAndCallRealMethodAnswer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
@@ -21,20 +19,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.verify;
+import static org.springframework.amqp.rabbit.test.RabbitListenerTestHarness.*;
 
 @SpringBootTest
-public class FooBarTests {
+public class FooBarTests2 {
 
     @Configuration
     @EnableRabbit
-    @RabbitListenerTest
+    @RabbitListenerTest(capture = true)
     public static class ListenerConfig {
         @Bean
-        public Listener Listener() {
+        public Listener listener() {
             return new Listener();
         }
 
@@ -83,44 +83,56 @@ public class FooBarTests {
     @Autowired
     private Queue queue2;
 
-
     @Test
     public void testTwoWay() throws Exception {
         assertEquals("FOO", this.rabbitTemplate.convertSendAndReceive(this.queue1.getName(), "foo"));
 
-        Listener listener = this.harness.getSpy("foo");
-        assertNotNull(listener);
-        // was called 1 time
-        verify(listener).foo("foo");
+        InvocationData invocationData =
+                this.harness.getNextInvocationDataFor("foo", 0, TimeUnit.SECONDS);
+        assertThat(invocationData.getArguments()[0]).isEqualTo("foo");
+        assertThat((String) invocationData.getResult()).isEqualTo("FOO");
     }
 
     @Test
     public void testOneWay() throws Exception {
-        Listener listener = this.harness.getSpy("bar");
-        assertNotNull(listener);
-
-        LatchCountDownAndCallRealMethodAnswer answer = this.harness.getLatchAnswerFor("bar", 2);
-        doAnswer(answer).when(listener).foo(anyString(), anyString());
-
         this.rabbitTemplate.convertAndSend(this.queue2.getName(), "bar");
         this.rabbitTemplate.convertAndSend(this.queue2.getName(), "baz");
+        this.rabbitTemplate.convertAndSend(this.queue2.getName(), "ex");
 
-        assertTrue(answer.await(10));
-        verify(listener).foo("bar", this.queue2.getName());
-        verify(listener).foo("baz", this.queue2.getName());
+        RabbitListenerTestHarness.InvocationData invocationData =
+                this.harness.getNextInvocationDataFor("bar", 10, TimeUnit.SECONDS);
+        Object[] args = invocationData.getArguments();
+        assertThat((String) args[0]).isEqualTo("bar");
+        assertThat((String) args[1]).isEqualTo(queue2.getName());
+
+        invocationData = this.harness.getNextInvocationDataFor("bar", 10, TimeUnit.SECONDS);
+        args = invocationData.getArguments();
+        assertThat((String) args[0]).isEqualTo("baz");
+
+        invocationData = this.harness.getNextInvocationDataFor("bar", 10, TimeUnit.SECONDS);
+        args = invocationData.getArguments();
+        assertThat((String) args[0]).isEqualTo("ex");
+        assertEquals("ex", invocationData.getThrowable().getMessage());
     }
 
-    @Slf4j
-    public static class Listener {
-        @RabbitListener(id="foo", queues="#{queue1.name}")
-        public String foo(String foo) {
-            return foo.toUpperCase();
-        }
 
-        @RabbitListener(id="bar", queues="#{queue2.name}")
-        public void foo(@Payload String foo, @Header("amqp_receivedRoutingKey") String rk) {
-            log.info("Received message: {}, routing key: {}", foo, rk);
+   public static class Listener {
+       private boolean failed;
 
-        }
-    }
+       @RabbitListener(id="foo", queues="#{queue1.name}")
+       public String foo(String foo) {
+           return foo.toUpperCase();
+       }
+
+       @RabbitListener(id="bar", queues="#{queue2.name}")
+       public void foo(@Payload String foo, @Header("amqp_receivedRoutingKey") String rk) {
+           if (!failed && foo.equals("ex")) {
+               failed = true;
+               throw new RuntimeException(foo);
+           }
+           failed = false;
+
+       }
+   }
+
 }
